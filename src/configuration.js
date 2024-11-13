375ca01dc323eb7290774e1f98c29751495b51fc
M <mt.dev.2023@gmail.com>
2024-11-13 02:42:12 +0100

    first commit

---
+++ src/configuration.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyA5ICdUEvrTshrmxNmgHiLA1RrXCNYKBJI",
+    authDomain: "auth-wbp-firebase.firebaseapp.com",
+    projectId: "auth-wbp-firebase",
+    storageBucket: "auth-wbp-firebase.firebasestorage.app",
+    messagingSenderId: "615417321906",
+    appId: "1:615417321906:web:7d252d53fc1430ef9a2cab"
+  };
